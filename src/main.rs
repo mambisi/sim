d41988a445ee4e2fc3cb761165c1743cd520dec5
mambisi <lilbizi96@gmail.com>
2021-05-31 15:56:15 +0000

    initail commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use notus::nutos::Notus;
+
+fn main() {
+    let n = 2_000_000;
+    let notus = Notus::temp("./temp_dir").unwrap();
+    for i in 0..n {
+        if i % 100_000 == 0 {
+            println!("Block Level {}", i);
+        }
+        let i = (i % u8::MAX as u32) as u8;
+        let key = vec![i;32];
+        let value = vec![i;32];
+        notus.put(key,value).unwrap();
+    }
+}
